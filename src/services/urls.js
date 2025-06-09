951c1b7caf7a10f1c54ddac20927dc0c1ca1ae8e
Diego <dieeid10@gmail.com>
2025-06-09 17:15:56 -0300

    Se arregla la url del backend

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,6 +1,6 @@
 import { IS_DEVELOPMENT } from "../config"
 
-const baseUrl = IS_DEVELOPMENT ? 'http://localhost:8000' : 'http://51.79.120.14:8000/'
+const baseUrl = IS_DEVELOPMENT ? 'http://localhost:8000' : 'http://51.79.120.14:8000'
 
 export const urls = {
     lectorCodeMrz: `${baseUrl}/get_data_mrz`,
